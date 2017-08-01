04b8c0defce9755081a74817c75d6b07bd5026f7
ys3669 <ys3669jp@gmail.com>
2017-08-02 02:43:57 +0900

    🍣
    床を1枚に
    散々言われてた当たり判定のタイミングの調整
    Startの実装

--- Assets/Script/Get_anime.cs
+++ Assets/Script/Get_anime.cs
@@ -12,14 +12,16 @@ public class Get_anime: MonoBehaviour {
 		AnimatorStateInfo stateInfo = mAnimator.GetCurrentAnimatorStateInfo(0);
 		// レイヤーのインデックスを引数に指定
 
-		if (stateInfo.IsName ("Base.stopEND")) {
-				this.GetComponent<BoxCollider> ().isTrigger = false;
-			//	Debug.Log("closeNow_false");
-		}
 		if( stateInfo.IsName("Base.open") ) {
-			
-			this.GetComponent<BoxCollider> ().isTrigger = true;
-			//	Debug.Log("closeNow_true");
+		this.GetComponent<BoxCollider> ().isTrigger = true;
+		//	Debug.Log("closeNow_true");
 		}
+		if (stateInfo.IsName ("Base.stopEND")) {
+		Invoke("RriggerF",0.2f);
+		}
+	}
+	void RriggerF (){
+			this.GetComponent<BoxCollider> ().isTrigger = false;
+			//	Debug.Log("closeNow_false");
 	}
 }
